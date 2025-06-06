35f7541f61c92eef7cd0dc8a83a2731f0c9380f0
Nikita89pog <nikitapogorelets@gmail.com>
2025-06-06 21:40:41 +0300

    Add files via upload

---
+++ California-1/js/main.js
@@ -0,0 +1,14 @@
+
+// Слайдер-шапка========================
+
+const swiper = new Swiper('.cap__slider', {
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
